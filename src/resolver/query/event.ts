a778d5295f866f687497f18941f72bfce4ab4498
BilguunLeap3 <bilguunleap3@iMac2021-02s-iMac.local>
2023-05-17 15:43:26 +0800

    added some aggregateion in event query

--- src/resolver/query/event.ts
+++ src/resolver/query/event.ts
@@ -3,14 +3,17 @@ import { Event } from '../../model/event';
 import aggregatePipeline from '../../utils/pipeline';
 
 export const events = async (_: any, { arg }: any) => {
-    let pipeline = aggregatePipeline(arg)
-    
-    const a = await Event.aggregate(pipeline);
-    a.map(b => b.category = b.category[0])
-    a.map(b => b.country = b.country[0])
-    a.map(b => b.city = b.city[0])
-    a.map(b => b.organizer = b.organizer[0])
-    return a;
+    if(arg) {
+        let pipeline = aggregatePipeline(arg);
+        const a = await Event.aggregate(pipeline);
+        a.map(b => b.category = b.category[0])
+        a.map(b => b.country = b.country[0])
+        a.map(b => b.city = b.city[0])
+        a.map(b => b.organizer = b.organizer[0])
+        return a;
+    }
+    const events = await Event.find().populate(['organizer', 'category']);
+    return events
 }
 export const event = async (_parent: any, args: any) => {
     const { id: eventid } = args;
--- src/utils/pipeline.ts
+++ src/utils/pipeline.ts
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 import { Category, Company } from "../model";
-import { Country } from "../model/country";
 import { City } from "../model/city";
+import { Country } from "../model/country";
 
 export default function aggregatePipeline(arg: any) {
     let pipeline: 
@@ -106,7 +106,7 @@ export default function aggregatePipeline(arg: any) {
         }
     },
 ]);
-    //query by date and country category
+    //query by date and country
     (arg.from && arg.to && arg.countryid) && (pipeline = [
         {
             $match: {
@@ -123,9 +123,35 @@ export default function aggregatePipeline(arg: any) {
             }
         },
         {
+            //@ts-ignore
+            $lookup: {
+                from: Country.collection.name,
+                localField: 'country',
+                foreignField: '_id',
+                as: 'country'
+            }
+        }
+    ]);
+    //query by date and country and city
+    (arg.from && arg.to && arg.countryid && arg.cityid) && (pipeline = [
+        {
+            $match: {
+                startDate: {
+                    $gt: new Date(arg?.from),
+                    $lt: new Date(arg?.to)
+                }
+            },
+        },
+        {
             $match: {
                 //@ts-ignore
-                category: new mongoose.Types.ObjectId(arg.categoryid)
+                country: new mongoose.Types.ObjectId(arg.countryid)
+            }
+        },
+        {
+            $match: {
+                //@ts-ignore
+                city: new mongoose.Types.ObjectId(arg.cityid)
             }
         },
         {
@@ -145,27 +171,9 @@ export default function aggregatePipeline(arg: any) {
                 foreignField: '_id',
                 as: 'country'
             }
-        },
-        {
-            //@ts-ignore
-            $lookup: {
-                from: City.collection.name,
-                localField: 'city',
-                foreignField: '_id',
-                as: 'city'
-            }
-        },
-        {
-            //@ts-ignore
-            $lookup: {
-                from: Category.collection.name,
-                localField: 'category',
-                foreignField: '_id',
-                as: 'category'
-            }
-        },
+        }
     ]);
-    //query by date and country and city
+    //query by date and country and city and categoryid
     (arg.from && arg.to && arg.countryid && arg.cityid && arg.categoryid) && (pipeline = [
         {
             $match: {
@@ -210,15 +218,18 @@ export default function aggregatePipeline(arg: any) {
                 foreignField: '_id',
                 as: 'country'
             }
-        },
+        }
+    ]);
+    ////////////
+    //query by includes string
+    (arg.includes) && (pipeline = [
         {
-            //@ts-ignore
-            $lookup: {
-                from: Category.collection.name,
-                localField: 'category',
-                foreignField: '_id',
-                as: 'category'
-            }
+            $match: {
+                //@ts-ignore
+                "title": {
+                    $in: [arg.includes]
+                }
+            },
         },
     ]);
     return pipeline
